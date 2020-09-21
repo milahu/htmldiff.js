8a5762e7acb103a5a003bd21f9c3758338d5064d
NatalieSantiago <natalie.santiago@davincitech.co>
2020-09-21 16:36:16 -0500

    ajuste para usar en angular

--- js/htmldiff.d.ts
+++ js/htmldiff.d.ts
@@ -17,4 +17,4 @@
  * @return {string} The combined HTML content with differences wrapped in `<ins>` and `<del>` tags.
  */
 declare function diff(before: string, after: string, className?: string | null, dataPrefix?: string | null, atomicTags?: string | null): string;
-export = diff;
+export default diff;
